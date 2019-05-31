573d1a504293d71591686708a7c2fba6a191961f
Tomasz Rożdżyński <trozdzynski@speednet.pl>
2019-05-31 16:24:34 +0200

    Examples updated

--- tutorial/docker-compose-example/services/ngrok/showtunnels.sh
+++
@@ -1,2 +0,0 @@
-NGROK_TUNNEL_URL=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
-echo "NGROK tunnel public url is $NGROK_TUNNEL_URL"
