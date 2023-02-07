0bc5808fcc8d601b0777e935687ba014b0035740
ChieftainY2k <ChieftainY2k@gmail.com>
2023-02-07 22:45:08 +0100

    update

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,7 +3,7 @@
 #helper function
 log_message()
 {
-    LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')][provision]"
+    LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')][$(basename $0)]"
     MESSAGE=$1
     echo "$LOGPREFIX $MESSAGE"
 }
@@ -15,10 +15,11 @@ check_errors()
     if [[ ${EXITCODE} -ne 0 ]]; then
         log_message "ERROR: there were some errors, check the output for details."
         exit 1
-    else
-        log_message "OK, operation successfully completed."
+#    else
+#        log_message "OK, operation successfully completed."
     fi
 }
+
 # update DNS
 #echo "nameserver 1.1.1.1" > /etc/resolv.conf
 #echo "nameserver 8.8.8.8" >> /etc/resolv.conf
@@ -29,47 +30,50 @@ check_errors $?
 sudo apt-get -y upgrade
 check_errors $?
 
+# Timezone config
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+check_errors $?
+
 # install common system libs
-sudo apt-get install -y mc joe git multitail screen nmap python-pip htop iotop jnettop elinks pydf zip
+sudo apt-get install -y mc joe git multitail screen nmap htop iotop jnettop elinks pydf zip docker-compose
 check_errors $?
 
-# install docker
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
-check_errors $?
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-check_errors $?
-
-sudo apt-key fingerprint 0EBFCD88
-check_errors $?
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-check_errors $?
-
-sudo apt-get update
-check_errors $?
-
-sudo apt-get install -y docker-ce
-check_errors $?
-
-#sudo docker run hello-world
+#curl https://get.docker.com/ | sh
 #check_errors $?
+
+sudo docker run hello-world
+check_errors $?
 
 sudo usermod -aG docker vagrant
 check_errors $?
 
-sudo pip install docker-compose
-check_errors $?
+##Update docker-compose to the newest version, see https://github.com/docker/for-linux/issues/563
+#sudo apt-get -y remove docker-compose golang-docker-credential-helpers
+#check_errors $?
+##VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+#VERSION="1.28.6"
+#DESTINATION=/usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+#check_errors $?
+#sudo chmod 755 $DESTINATION
+#check_errors $?
+
+#sudo pip install docker-compose
+#check_errors $?
 
 docker-compose version
 check_errors $?
 
+sudo usermod -aG docker ${USER}
+check_errors $?
 
+#sudo mkdir /tmp/composer-cache
+#check_errors $?
+#sudo chmod -R a+rwxX /tmp/composer-cache
+#check_errors $?
+
+# Docker environment config
+#echo "export DOCKER_ENV=local" >> /home/vagrant/.bashrc
+echo "export DOCKER_BUILDKIT=1" >> /home/vagrant/.bashrc
+echo "export PROMPT_COMMAND=\"history -a; history -r\"" >> /home/vagrant/.bashrc
+check_errors $?
